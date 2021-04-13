4d53536db78b3366593df73d4b2fc85777acea78
Kaz <kathryngoulding92@gmail.com>
2021-04-13 18:21:58 +0100

    Added Python, HTML and requirements files, and code within the files

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask( __name__ )
+
+from sqlalchemy import create_engine
+import sqlalchemy.orm
+from database_setup import Base
+
+engine = create_engine( "mysql://admin1:@GitPa$$w0rd#@54.74.234.11/team_404?charset=utf8mb4" )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin1:@GitPa$$w0rd#@54.74.234.11/team_404'
+Base.metadata.bind = engine
+
+DBSession = sqlalchemy.orm.sessionmaker( bind=engine )
+session = DBSession()
+
+# @app.route( '/' )
+# @app.route( '/blog' )
+# def showBooks():
+#     books = session.query( Book ).all()
+#     return render_template( "books.html", books=books )
+#
+#
+# # This will let us Create a new book and save it in our database
+# @app.route( '/books/new/', methods=['GET', 'POST'] )
+# def newBook():
+#     if request.method == 'POST':
+#         newBook = Book( title=request.form['name'], author=request.form['author'], genre=request.form['genre'] )
+#         session.add( newBook )
+#         session.commit()
+#         return redirect( url_for( 'showBooks' ) )
+#     else:
+#         return render_template( 'createbook.html' )
+#
+#
+# # This will let us Update our books and save it in our database
+# @app.route( "/books/<int:book_id>/edit/", methods=['GET', 'POST'] )
+# def editBook(book_id):
+#     editedBook = session.query( Book ).filter_by( id=book_id ).one()
+#     if request.method == 'POST':
+#         if request.form['name']:
+#             editedBook.title = request.form['name']
+#             return redirect( url_for( 'showBooks' ) )
+#     else:
+#         return render_template( 'editBook.html', book=editedBook )
+#
+#
+# # This will let us Delete our book
+# @app.route( '/books/<int:book_id>/delete/', methods=['GET', 'POST'] )
+# def deleteBook(book_id):
+#     bookToDelete = session.query( Book ).filter_by( id=book_id ).one()
+#     if request.method == 'POST':
+#         session.delete( bookToDelete )
+#         session.commit()
+#         return redirect( url_for( 'showBooks', book_id=book_id ) )
+#     else:
+#         return render_template( 'deleteBook.html', book=bookToDelete )
+#
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run( host='localhost', port=5555)
---
+++ database_setup.py
@@ -0,0 +1,35 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String, MetaData, Table, ForeignKeyConstraint, DateTime, \
+    text
+
+from sqlalchemy.schema import ForeignKeyConstraint
+
+from sqlalchemy.dialects.mysql import \
+        BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, \
+        DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, \
+        LONGBLOB, LONGTEXT, MEDIUMBLOB, MEDIUMINT, MEDIUMTEXT, NCHAR, \
+        NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, \
+        TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR, insert
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+meta = MetaData()
+
+
+class Blog (Base):
+    __tablename__ = 'Blog_post'
+
+    author = Column(String(250), primary_key=True)
+    title = Column(String(250), nullable=False)
+    contents = Column(String(VARCHAR), nullable=False)
+    date = Column(DateTime, nullable=False)
+
+# creates a create_engine instance - insert our password and team
+engine = create_engine( "mysql://admin1:@GitPa$$w0rd#@54.74.234.11/team_404?charset=utf8mb4" )
+
+
+Base.metadata.create_all(engine)
