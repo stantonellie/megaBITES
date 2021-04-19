8c2c6e42a26d2f34e703971dac7b68bfdde0c374
Kaz <kathryngoulding92@gmail.com>
2021-04-19 14:44:44 +0100

    commented out elements we arent using yet (login, registration, app routes for login and registration) issue: app.route('/') wont work for me?

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ def recipes():
 def contact():
     return render_template("contact.html")
 
-@app.route('/login/')
-def login():
-    return render_template("login.html")
-
-@app.route('/registration/')
-def registration():
-    return render_template("registration.html")
+# @app.route('/login/')
+# def login():
+#     return render_template("login.html")
+#
+# @app.route('/registration/')
+# def registration():
+#     return render_template("registration.html")
 
 
 
