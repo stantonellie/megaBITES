1d2cb2974c6b83f20593fdbdd30e4a9ccd74afaa
Kaz <kathryngoulding92@gmail.com>
2021-04-19 13:27:45 +0100

    altered login and registration html files, and added routes to main.py

--- main.py
+++ main.py
@@ -32,6 +32,14 @@ def recipes():
 def contact():
     return render_template("contact.html")
 
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+@app.route('/registration/')
+def registration():
+    return render_template("registration.html")
+
 
 
 # @app.route( '/' )
